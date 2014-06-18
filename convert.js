0fb5156ec469ff9b843959deced66683cac1a326
Ivo Wetzel <ivo.wetzel@googlemail.com>
2014-06-19 01:21:30 +0200

    Implement new compression routines which are faster than lz4 and achieve better compression than both lz4 and the old packer routine

--- convert.js
+++ convert.js
@@ -311,8 +311,282 @@ var Pack = {
 
     }
 
-
 };
+
+(function(exports) {
+
+    // Constants --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    var maxRepeatCount = 33,
+        minRepeatLength = 2,
+        maxLiteralLength = 128,
+        maxCopyLength = 66,
+        minCopyLength = 3,
+        maxCopyOffset = 255;
+
+
+    // Data Analyzers ---------------------------------------------------------
+    // ------------------------------------------------------------------------
+    function findSingleRepeat(buf, from, to) {
+
+        var s = buf[from],
+            len = 0;
+
+        for(var i = from; i < to; i++) {
+            if (buf[i] === s) {
+                len++;
+
+                if (len === maxRepeatCount) {
+                    break;
+                }
+
+            } else {
+                break;
+            }
+        }
+
+        return len < minRepeatLength ? -1 : len;
+
+    }
+
+    function findDualRepeat(buf, from, to) {
+
+        var s = buf[from],
+            t = buf[from + 1],
+            len = 0;
+
+        for(var i = from; i < to; i += 2) {
+            if (buf[i] === s && buf[i + 1] === t) {
+                len++;
+                if (len === maxRepeatCount) {
+                    break;
+                }
+
+            } else {
+                break;
+            }
+        }
+
+        return len < minRepeatLength ? -1 : len;
+
+    }
+
+    function findSubCopy(buf, from, length) {
+
+        for(var at = from - length; at >= Math.max(from - maxCopyOffset, 0) ; at--) {
+
+            // Check for maximum copy length
+            var match = true;
+            for(var i = 0; i < length; i++) {
+                if (buf[from + i] !== buf[at + i]) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                return from - at - length;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    function findCopy(buf, from) {
+
+        findCopy.value.length = -1;
+
+        for(var length = minCopyLength; length < Math.min(maxCopyLength, buf.length - from + 1); length++) {
+
+            var at = findSubCopy(buf, from, length, 0);
+            if (at !== -1) {
+                findCopy.value.offset = at;
+                findCopy.value.length = length;
+
+            } else {
+                break;
+            }
+
+        }
+
+        return findCopy.value;
+
+    }
+
+    findCopy.value = {
+        length: -1,
+        offset: 0
+    };
+
+
+
+    // Encoder ----------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    var copyEncodeLength = 2,
+        singleEncodeLength = 2,
+        dualEncodeLength = 3;
+
+    function encode(data, addSizePrefix, debug) {
+
+        var literalCount = 0,
+            index = 0,
+            output = [];
+
+        while(index < data.length) {
+
+            var singleRepeat = findSingleRepeat(data, index, index + maxRepeatCount),
+                dualRepeat = findDualRepeat(data, index, index + maxRepeatCount * 2),
+                copy = findCopy(data, index);
+
+            var savedSingle = singleRepeat * 1 - singleEncodeLength,
+                savedDual = dualRepeat * 2 - dualEncodeLength,
+                savedCopy = copy.length - copyEncodeLength;
+
+            if (savedSingle > 0 || savedDual > 0 || savedCopy > 0 || literalCount === maxLiteralLength) {
+                if (literalCount > 0) {
+
+                    // 1 0000000
+                    //   1-128 literals
+                    output.push(((literalCount - 1) & 0x7f) | 0x80);
+                    output.push.apply(output, data.slice(index - literalCount, index));
+
+                    literalCount = 0;
+
+                }
+            }
+
+            // Single Repeat
+            if (savedSingle > 0 && savedSingle >= savedDual && savedSingle >= savedCopy) {
+
+                if (singleRepeat > maxRepeatCount) {
+                    throw new Error('Single Repeat Count out of Range: ' + singleRepeat);
+                }
+
+                // 01 0 00000
+                // repeat the next 1-2 bytes 2-33 times
+                output.push(0x40 | ((singleRepeat - minRepeatLength) & 0x1f));
+                output.push(data[index]);
+
+                index += singleRepeat;
+
+            // Dual Repeat
+            } else if (savedDual > 0 && savedDual >= savedSingle && savedDual >= savedCopy) {
+
+                if (dualRepeat > maxRepeatCount) {
+                    throw new Error('Dual Repeat Count out of Range: ' + dualRepeat);
+                }
+
+                // 01 1 00000
+                // repeat the next 1-2 bytes 2-33 times
+                output.push(0x40 | 0x20 | ((dualRepeat - minRepeatLength) & 0x1f));
+                output.push(data[index]);
+                output.push(data[index + 1]);
+
+                index += dualRepeat * 2;
+
+            // Copy
+            } else if (savedCopy > 0 && savedCopy) {
+
+                if (copy.length > maxCopyLength) {
+                    throw new Error('Copy Length out of Range: ' + copy.length);
+                }
+
+                if (copy.length > maxCopyOffset) {
+                    throw new Error('Copy Offset out of Range: ' + copy.offset);
+                }
+
+                // 00 000000 00000000
+                // copy 3-67 bytes from offset 1-256 (+length)
+                output.push((copy.length - minCopyLength) & 0x3f);
+                output.push(copy.offset & 0xff);
+
+                index += copy.length;
+
+            } else {
+                literalCount++;
+                index++;
+            }
+
+        }
+
+        if (literalCount > 0) {
+            output.push(((literalCount - 1) & 0x7f) | 0x80);
+            output.push.apply(output, data.slice(index - literalCount, index));
+        }
+
+        if (addSizePrefix !== false) {
+            var size = data.length;
+            return [(size >> 8), size & 0xff].concat(output);
+
+        } else {
+            return output;
+        }
+
+    }
+
+    function decode(data) {
+
+        var output = [],
+            index = 0;
+
+        while(index < data.length) {
+
+            var type = data[index],
+                i = 0,
+                offset = 0,
+                length = 0;
+
+            // Literal Data
+            if ((type & 0x80) === 0x80) {
+                length = (type & 0x7f) + 1;
+                output.push.apply(output, data.slice(index + 1, index + 1 + length));
+                index += length + 1;
+
+            // Repeat
+            } else if ((type & 0x40) === 0x40) {
+
+                length = (type & 0x1f) + minRepeatLength;
+
+                // Dual
+                if ((type & 0x20) === 0x20) {
+
+                    for(i = 0; i < length; i++) {
+                        output.push(data[index + 1], data[index + 2]);
+                    }
+                    index += 3;
+
+                // Single
+                } else {
+                    for(i = 0; i < length; i++) {
+                        output.push(data[index + 1]);
+                    }
+                    index += 2;
+                }
+
+            // Copy
+            } else {
+                length = (type & 0x3f) + minCopyLength;
+                offset = data[index + 1];
+
+                var origin = output.length;
+                for(i = 0; i < length; i++) {
+                    output.push(output[(origin - offset - length) + i]);
+                }
+                index += 2;
+            }
+
+        }
+
+        return output;
+
+    }
+
+    exports.encode = encode;
+    exports.decode = decode;
+
+})(Pack);
 
 
 // Data Parsing and Conversion ------------------------------------------------
@@ -341,7 +615,7 @@ var Parse = {
     },
 
     /** Encode a image of 16x16px tiles into a mapping of rows along with a index header. */
-    rowMapFromImage: function(palette, columns, img, file) {
+    rowMapFromImage: function(palette, columns, img) {
 
         var rowOffsets = [],
             rowBytes = [];
@@ -358,7 +632,8 @@ var Parse = {
 
             var tileBytes = Parse.tilesFromImage(palette, true, subImage);
             rowOffsets.push((rowBytes.length >> 8), rowBytes.length & 0xff);
-            rowBytes.push.apply(rowBytes, Pack.pack(tileBytes, false, y === 3 && file === 'entities.ch.png'));
+            //console.log(Pack.pack(tileBytes, false).length, Pack.encode(tileBytes).length);
+            rowBytes.push.apply(rowBytes, Pack.pack(tileBytes, false));
 
         }
 
@@ -602,7 +877,7 @@ var Map = {
         animations = Map.parseAnimations(tileBytes, blockDefinitions, animationOffset);
 
         // Pack Tile Data
-        tiles = Pack.pack(tileBytes, false);
+        tiles = Pack.encode(tileBytes, false);//Pack.pack(tileBytes, false);
 
         if (animations.used) {
             header |= 1;
@@ -705,7 +980,8 @@ var Map = {
             used: entityIndex !== -1
         };
 
-    },
+    }
+
 };
 
 
@@ -741,7 +1017,10 @@ var Convert = {
             return Parse.tilesFromImage(palette, r8x16, img);
 
         }).then(function(bytes) {
-            return Pack.lz4Sync(bytes);
+
+            //console.log(Pack.lz4Sync(bytes).length, Pack.encode(bytes).length);
+            var b = Pack.encode(bytes, false, true);
+            return Pack.encode(bytes, false);
 
         }).then(function(data) {
             return IO.saveAs('bin', file, data);
@@ -1154,7 +1433,16 @@ IO.setDest(path.join(process.cwd(), process.argv[3]));
 
 
 // Convert --------------------------------------------------------------------
-if (process.argv[4] === '-reverse') {
+if (process.argv[4] === '-test') {
+
+    var data = [];
+    for(var i = 0, l = 80; i < l; i++) {
+        data.push(0);
+    }
+
+    console.log(Pack.encode(data));
+
+} else if (process.argv[4] === '-reverse') {
     Reverse.TileDef();
 
 } else {
