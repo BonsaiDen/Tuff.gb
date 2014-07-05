625b27f6889f05c6465e2c88ec6bb735993d14e1
Ivo Wetzel <ivo.wetzel@googlemail.com>
2014-07-05 19:29:13 +0200

    DRY some of the collision code

--- src/game/player/collision.gb.s
+++ src/game/player/collision.gb.s
@@ -2,53 +2,6 @@
 
 
 ; UP --------------------------------------------------------------------- UP -
-player_collision_up: 
-
-    ; different height when diving
-    ld      a,[playerUnderWater]
-    cp      1
-    jr      z,.diving
-    ld      e,PLAYER_HEIGHT
-    jr      .check
-
-.diving:
-    ld      e,PLAYER_HEIGHT + 2
-
-.check:
-    ; middle
-    ld      a,[playerY]
-    sub     e
-    ld      c,a
-
-    ld      a,[playerX]
-    ld      b,a
-    call    map_get_collision
-    cp      1
-    ret     z
-
-    ; right half 
-    ld      a,[playerY]
-    sub     e
-    ld      c,a
-
-    ld      a,[playerX]
-    add     PLAYER_HALF_WIDTH - 3
-    ld      b,a
-    call    map_get_collision
-    cp      1
-    ret     z
-
-    ; left half 
-    ld      a,[playerY]
-    sub     e
-    ld      c,a
-
-    ld      a,[playerX]
-    sub     PLAYER_HALF_WIDTH - 2
-    ld      b,a
-    call    map_get_collision
-    ret
-
 player_collision_far_up: 
 
     ; different height when diving
@@ -56,12 +9,25 @@ player_collision_far_up:
     cp      1
     jr      z,.diving
     ld      e,PLAYER_HEIGHT + 3
-    jr      .check
+    jr      _player_col_up
 
 .diving:
     ld      e,PLAYER_HEIGHT + 5
+    jr      _player_col_up
 
-.check:
+
+player_collision_up: 
+    ; different height when diving
+    ld      a,[playerUnderWater]
+    cp      1
+    jr      z,.diving
+    ld      e,PLAYER_HEIGHT
+    jr      _player_col_up
+
+.diving:
+    ld      e,PLAYER_HEIGHT + 2
+
+_player_col_up:
     ; middle
     ld      a,[playerY]
     sub     e
@@ -183,52 +149,21 @@ _player_col_check_down:
 
 
 ; LEFT ----------------------------------------------------------------- LEFT -
-player_collision_left:
-
-    ; middle
-    ld      a,[playerY]
-    sub     a,7
-    ld      c,a
-
-    ld      a,[playerX]
-    sub     PLAYER_HALF_WIDTH - 1
-    ld      b,a
-    call    map_get_collision
-    cp      1
-    ret     z
-
-    ; upper half 
-    ld      a,[playerY]
-    sub     a,PLAYER_HEIGHT - 1
-    ld      c,a
-
-    ld      a,[playerX]
-    sub     PLAYER_HALF_WIDTH - 1
-    ld      b,a
-    call    map_get_collision
-    cp      1
-    ret     z
-
-    ; lower half 
-    ld      a,[playerY]
-    sub     1
-    ld      c,a
-
-    ld      a,[playerX]
-    sub     PLAYER_HALF_WIDTH - 1
-    ld      b,a
-    call    map_get_collision
-    ret     
-
 player_collision_far_left:
+    ld      h,8
+    jr      _player_col_left
 
+player_collision_left:
+    ld      h,PLAYER_HALF_WIDTH - 1
+
+_player_col_left:
     ; middle
     ld      a,[playerY]
     sub     a,7
     ld      c,a
 
     ld      a,[playerX]
-    sub     8
+    sub     h
     ld      b,a
     call    map_get_collision
     cp      1
@@ -240,7 +175,7 @@ player_collision_far_left:
     ld      c,a
 
     ld      a,[playerX]
-    sub     8
+    sub     h
     ld      b,a
     call    map_get_collision
     cp      1
@@ -252,7 +187,7 @@ player_collision_far_left:
     ld      c,a
 
     ld      a,[playerX]
-    sub     8
+    sub     h
     ld      b,a
     call    map_get_collision
     ret     
@@ -296,52 +231,21 @@ player_collision_left_all:
 
 
 ; RIGHT  -------------------------------------------------------------- RIGHT -
-player_collision_right:
-
-    ; middle
-    ld      a,[playerY]
-    sub     a,7
-    ld      c,a
-
-    ld      a,[playerX]
-    add     PLAYER_HALF_WIDTH - 2
-    ld      b,a
-    call    map_get_collision
-    cp      1
-    ret     z
-
-    ; upper half 
-    ld      a,[playerY]
-    sub     a,PLAYER_HEIGHT - 1
-    ld      c,a
-
-    ld      a,[playerX]
-    add     PLAYER_HALF_WIDTH - 2
-    ld      b,a
-    call    map_get_collision
-    cp      1
-    ret     z
-
-    ; lower half 
-    ld      a,[playerY]
-    sub     1
-    ld      c,a
-
-    ld      a,[playerX]
-    add     PLAYER_HALF_WIDTH - 2
-    ld      b,a
-    call    map_get_collision
-
-    ret
-
 player_collision_far_right:
+    ld      h,7
+    jr      _player_col_right
+
+player_collision_right:
+    ld      h,PLAYER_HALF_WIDTH - 2
+
+_player_col_right:
     ; middle
     ld      a,[playerY]
     sub     a,7
     ld      c,a
 
     ld      a,[playerX]
-    add     7
+    add     h
     ld      b,a
     call    map_get_collision
     cp      1
@@ -353,7 +257,7 @@ player_collision_far_right:
     ld      c,a
 
     ld      a,[playerX]
-    add     7
+    add     h
     ld      b,a
     call    map_get_collision
     cp      1
@@ -365,9 +269,10 @@ player_collision_far_right:
     ld      c,a
 
     ld      a,[playerX]
-    add     7
+    add     h
     ld      b,a
     call    map_get_collision
+
     ret
 
 player_collision_right_all:
