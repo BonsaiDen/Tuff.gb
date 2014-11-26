5ea62a3cd0315e8196296a1c854e3cfd414fe297
Ivo Wetzel <ivo.wetzel@googlemail.com>
2014-11-26 22:05:36 +0100

    Move save constants into RAM header

--- src/game/ram/save.gb.s
+++ src/game/ram/save.gb.s
@@ -1,5 +1,23 @@
 SECTION "SaveRam",RAM[$A000]
 
+; Constants -------------------------------------------------------------------
+SAVE_GAME_VERSION          EQU 9
+
+SAVE_HEADER_SIZE           EQU 3
+SAVE_VERSION_SIZE          EQU 1
+SAVE_CHECKSUM_SIZE         EQU 2
+SAVE_PLAYER_DATA_SIZE      EQU 12
+SAVE_ENTITY_HEADER_SIZE    EQU 1
+SAVE_ENTITY_DATA_SIZE      EQU ENTITY_STORED_STATE_SIZE
+SAVE_COMPLETE_SIZE         EQU SAVE_HEADER_SIZE + SAVE_VERSION_SIZE + SAVE_PLAYER_DATA_SIZE + SAVE_ENTITY_HEADER_SIZE + SAVE_ENTITY_DATA_SIZE
+
+SAVE_DEFAULT_PLAYER_X      EQU 12
+SAVE_DEFAULT_PLAYER_Y      EQU 48
+SAVE_DEFAULT_ROOM_X        EQU 3
+SAVE_DEFAULT_ROOM_Y        EQU 2
+SAVE_RAM_BANK              EQU 0
+
+
 ; Placeholder for save data ---------------------------------------------------
 saveData:           DS SAVE_COMPLETE_SIZE
 
